d750d6fae0168d716574a014aa691320919bea95
Anusha Syeda <anoshasyeda1999@gmail.com>
2025-04-12 18:06:58 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { auth } from "@/auth";
+"use client";
+
+import { useSession } from "next-auth/react";
 import { LoginButton } from "@/components/auth/login-button";
 import { LogoutButton } from "@/components/auth/logout-button";
 
-export default async function Home() {
-  const session = await auth();
+export default function Home() {
+  const { data: session } = useSession();
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
