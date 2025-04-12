941a45991d5032a8af3279ccd6ff07a43caccac7
Anusha Syeda <anoshasyeda1999@gmail.com>
2025-04-12 17:42:57 +0500

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -10,13 +10,26 @@ export const {
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? "",
       clientSecret: process.env.GITHUB_SECRET ?? "",
+      authorization: {
+        params: {
+          scope: "read:user user:email",
+        },
+      },
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID ?? "",
       clientSecret: process.env.GOOGLE_SECRET ?? "",
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
     }),
   ],
   pages: {
     signIn: "/signin",
   },
+  debug: process.env.NODE_ENV === "development",
 });
